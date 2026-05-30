095e1a5ef2ff7c0edde946b9fbdf35dbbb0fafe0
Toshy0111 <suwalkatoshy63@gmail.com>
2026-05-30 23:32:57 +0530

    Minimum Stack

---
+++ Stacks/minimumStack.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class MinStack {
+   
+    List <int[]> s;
+    public MinStack() {
+        s = new ArrayList<>();
+    }
+    public void push(int val) {
+        if (s.isEmpty()) {
+            s.add(new int[]{val, val});
+        } else {
+            int minVal = Math.min(val, s.get(s.size() - 1)[1]);
+            s.add(new int[]{val, minVal});
+        }
+    }
+    public void pop() {
+        s.remove(s.size() - 1);
+    }
+    public int top() {
+        return s.get(s.size() - 1)[0];
+    }
+    public int getMin() {
+        return s.get(s.size() - 1)[1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
